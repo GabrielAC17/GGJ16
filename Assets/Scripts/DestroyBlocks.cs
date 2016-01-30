059acb584a805420db2958cc80b023675f54f1c9
alisson.krul@hotmail.com <08071996>
2016-01-30 17:19:03 -0200

    feat(DestroyBlocks)

---
+++ Assets/Scripts/DestroyBlocks.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyBlocks : MonoBehaviour {
+	public void destroyBlocks(int blockType){
+
+	}
+	// Use this for initialization
+	/*
+	 * 1- Agua
+	 * 2- Fogo
+	 * 3- Terra
+	 * 4- Ar
+ 	*/
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
