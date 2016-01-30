7f17ef88a8a5dc03a90142c732a7adf658f237c2
GabrielAC17 <gabriel.de.ac@hotmail.com>
2016-01-30 15:51:20 -0200

    KeyCombo
    - Adicionado script funcional de combos de teclas (com cena de exemplo)

---
+++ Assets/Test.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+    private KeyCombo falconPunch = new KeyCombo(new string[] { "B1", "B2", "B3" , "B1"});
+    private KeyCombo falconKick = new KeyCombo(new string[] { "B3", "B2", "B1", "B1" });
+
+    void Update()
+    {
+        if (falconPunch.Check())
+        {
+            // do the falcon punch
+            Debug.Log("PUNCH");
+        }
+        if (falconKick.Check())
+        {
+            // do the falcon punch
+            Debug.Log("KICK");
+        }
+    }
+}
