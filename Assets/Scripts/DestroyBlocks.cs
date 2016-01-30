ff2c262d64b2f715c062874e492a96da37e601a0
alisson.krul@hotmail.com <08071996>
2016-01-30 18:55:50 -0200

    feat(destroyBlocks)

--- Assets/Scripts/DestroyBlocks.cs
+++ Assets/Scripts/DestroyBlocks.cs
@@ -2,9 +2,27 @@
 using System.Collections;
 
 public class DestroyBlocks : MonoBehaviour {
+	private bool isCombo = false;
+	private bool allBlocksVerified = true;
+	private ArrayList listToRemove= new ArrayList();
+	private ArrayList allList= new ArrayList();
 	public void destroyBlocks(int blockType){
-		Debug.Log ("typeBlock to destroy"+blockType);
+		Debug.Log ("typeBlock to destroy: " + blockType);
+		foreach (Collider2D obj in allList) {
+			Destroy (obj.gameObject);
+		}
+		allList= new ArrayList();
 	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		Debug.Log ("TriggerStay");
+		if (!allList.Contains (other)) {
+			allList.Add (other);
+			Debug.Log ("Block "+ other.gameObject.GetInstanceID() + " added!!");
+		}
+
+	}
+			
 	// Use this for initialization
 	/*
 	 * 1- Agua
@@ -18,6 +36,6 @@ public class DestroyBlocks : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
