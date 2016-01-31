0da52160d98447c916b83755543c2ee1012dba57
alisson.krul@hotmail.com <08071996>
2016-01-31 01:25:19 -0200

    fix physics

--- Assets/Scripts/SeedTotens.cs
+++ Assets/Scripts/SeedTotens.cs
@@ -5,6 +5,7 @@ public class SeedTotens : MonoBehaviour {
 	public GameObject[] prefabBlocks; 
 	public float distanceX = 5;
 	public float positionY = 10;
+	public float z=11;
 	private int[][] totens ={new int[]{1,2,4,2,3,4,2,3,1,4,3,2},new int[]{2,3,4,1,2,3,4},new int[]{3,4,1,2,3,4,3,2,2},new int[]{4,1,2,3,4,1,2,3,4}};
 	// Use this for initialization
 	void Start () {
@@ -13,9 +14,10 @@ public class SeedTotens : MonoBehaviour {
 			var totensSequence = totens[totensIndx];
 			for(var blockIndx=0;blockIndx<totensSequence.Length; blockIndx++){
 				var blockType = totens[totensIndx][blockIndx];
-				var gameObject = Instantiate(prefabBlocks[blockType-1],new Vector3(positionsX[totensIndx], positionY+(blockIndx*(5+blockIndx*(float)0.2)), 0), Quaternion.identity) as GameObject;
+				var gameObject = Instantiate(prefabBlocks[blockType-1],new Vector3(positionsX[totensIndx], positionY+(blockIndx*4), 0), Quaternion.identity) as GameObject;
 				DefaultBlock obj = gameObject.GetComponent<DefaultBlock> ();
 				obj.setType(blockType);
+				obj.transform.position = new Vector3 (obj.transform.position.x, obj.transform.position.y, (z + (-0.1f * blockIndx)));
 			}
 		}
 	}
